a83c95384d52d8c309578b4f369189d755fda637
Jason Byer <jbyer3@gmail.com>
2019-01-10 09:37:03 -0500

    floop

--- game.sh
+++ game.sh
@@ -1,49 +1,50 @@
-# Creates a variable
-# Does String Comparison
-# Uses Conditionals
-# Uses Loops
-# Does Simple Math with User Input
-
 #!/bin/bash
-colors=('red' 'orange' 'green' 'purple' 'black' 'white' 'silver' 'grey')
-
-echo ${colors[0]}
+myFavColor="blue"
 
 printf "Whats your favorite color?  "
-read userColor
+read userFavColor
+  if [[ "$userFavColor" == "$myFavColor" ]] 
+  then
+  echo "Hey, we have the same favorite color! $myFavColor"
+  else
+  echo "I really wish you had said $myFavColor instead of $userFavColor"
+fi
 
-echo ${userColor}
-# read userNumber
-#   if [ $userNumber -gt 0 ]
-#   then echo "$userNumber is a positive number"
-#   echo "================="
-#     while [ $counter -le $userNumber ]    
-#     do
-#       even=$counter%2
-#       if [[ "$even" -eq 0 ]]
-#         then echo "$counter is the current number, and its even"
-#         result="even"
-#       else 
-#         echo "$counter is the current number, and its odd"
-#         result="odd"
-#       fi
-#       ((counter++))
-#     done
-#   else 
-#   echo "$userNumber is a negative number"
-#   echo "================="
-#     while [ $counter -ge $userNumber ]
-#     do
-#       even=$counter%2
-#       if [[ "$even" -eq 0 ]]
-#         then echo "$counter is the current number, and its even"
-#         result="even"
-#       else 
-#         echo "$counter is the current number, and its odd"
-#         result="odd"
-#       fi
-#       ((counter--))
-#     done
-#   fi
+counter=0
 
-#   echo "User Number was $userNumber, which is fine if you $result numbers"
+printf "Pick a number!  "
+read userNumber
+echo $userNumber
+  if [ $userNumber -gt 0 ]
+  then echo "$userNumber is a positive number"
+  echo "================="
+    while [ $counter -le $userNumber ]    
+    do
+      even=$counter%2
+      if [[ "$even" -eq 0 ]]
+        then echo "$counter is the current number, and its even"
+        result="even"
+      else 
+        echo "$counter is the current number, and its odd"
+        result="odd"
+      fi
+      ((counter++))
+    done
+  else 
+  echo "$userNumber is a negative number"
+  echo "================="
+    while [ $counter -ge $userNumber ]
+    do
+      even=$counter%2
+      if [[ "$even" -eq 0 ]]
+        then echo "$counter is the current number, and its even"
+        result="even"
+      else 
+        echo "$counter is the current number, and its odd"
+        result="odd"
+      fi
+      ((counter--))
+    done
+  fi
+
+  echo "User Number was $userNumber, which is fine if you $result numbers"
